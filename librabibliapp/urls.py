c416212d8f83d75209011b7fdabe5c54749280e9
wunluxsze <pushokmaster@gmail.com>
2022-12-26 15:02:08 +0300

    urls

--- librabibliapp/urls.py
+++ librabibliapp/urls.py
@@ -8,6 +8,7 @@ from .views import LibraryEditView
 from .views import LibraryDeleteView
 
 urlpatterns = [
+    path('', LibraryView.as_view()),
     path('create/', LibraryAddView.as_view()),
     path('edit/int<id>', LibraryEditView.as_view()),
     path('delete/int<id>', LibraryDeleteView.as_view()),
