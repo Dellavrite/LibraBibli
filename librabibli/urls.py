8692948ce256f8c106e24ceefdbaa8f73a4ecfb3
wunluxsze <pushokmaster@gmail.com>
2022-12-26 13:48:54 +0300

    templates

--- librabibli/urls.py
+++ librabibli/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from librabibliapp.views import LibraryView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LibraryView.as_view())
 ]
