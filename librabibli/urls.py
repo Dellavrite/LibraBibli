b7f51ad415ca734c8fded2e49416f5de4f5e36df
lowage24 <dmitriy241006@gmail.com>
2022-12-26 14:48:59 +0300

    auto

--- librabibli/urls.py
+++ librabibli/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from librabibliapp.views import LibraryView
+from django.urls import path, include
+
+from LibraBibli.librabibliapp.views import LibraryDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', LibraryView.as_view()),
-    path('create/', LibraryAddView.as_view()),
-    path('edit/int<id>', LibraryEditView.as_view()),
-    path
+    path('', include("librabibliapp.urls"))
+
 ]
