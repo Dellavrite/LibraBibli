093bfb152b749888f45633b833acbfc982120a81
PedroRusi <vovaamukov@gmail.com>
2022-12-26 13:40:26 +0300

    view

--- librabibliapp/views.py
+++ librabibliapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LibraryView(View):
+
+     def get(self, request):
+         return render(request, 'index.html')
+
+
+class LibraryAddView(View):
+
+    def get(self, request):
+        return render(request, 'create.html')
