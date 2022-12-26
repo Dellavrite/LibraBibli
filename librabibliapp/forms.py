01ae729308533b911935914025c603da126ce928
Dellavrite <delavrite@gmail.com>
2022-12-26 14:05:21 +0300

    auto

--- librabibliapp/forms.py
+++ librabibliapp/forms.py
@@ -8,3 +8,4 @@ class OrderForm(forms.Form):
     book = ModelChoiceField(label="Книга", queryset=Book.objects.all(), to_field_name="name")
     person_name = forms.CharField(label="ФИО")
     phone = forms.CharField(label="Номер телефона", widget=forms.NumberInput)
+
